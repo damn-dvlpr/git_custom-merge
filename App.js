1373ed22534716899294648358e161334ec8c14f
Piyushkumarraja <43479446+Piyushkumarraja@users.noreply.github.com>
2021-10-20 20:07:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -11,8 +11,45 @@ state= {
 
   ]
 };
-
-
+  
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+var cookieParser = require("cookie-parser");
+const fs=require("file-system");
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/views"));
+app.use(cookieParser());
+app.use(function(req,res,next){
+  var date=Date(Date.now()).toString();
+  console.log(date.slice(8,25), req.method, req.originalUrl);
+  // if(req.originalUrl=="/shop"&&req.method=="POST"){
+  //     console.log(res.body.returnedData);
+  // }
+  fs.appendFile('./log/logs.txt',
+  date + ` ${req.method} ${req.originalUrl} \n`
+  , function(err) {
+      if(!err){
+          next();
+      }
+      else console.log(err);
+  });
+});
+mongoose.connect("mongodb://localhost:27017/polling");
+var TeamSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+    unique: true,
+    required:true
+  },
+  Members: [{ type: String }],
+  votes: {
+    type: Number,
+    default: 0
+  }
+});
 
   render() {
     return (
