af2ff97af720bc2342f4143a13fd965182ef4a92
Ravi Kumar <86494864+ravi-2012@users.noreply.github.com>
2021-10-31 23:39:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -13,6 +13,26 @@ const jest = require('jest');
 const argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
+
+alert("connected");
+var body=document.querySelector("body");
+var button=document.querySelector("button");
+var h2=document.querySelector("h2");
+
+var red=false;
+button.addEventListener("click",function(){
+    if(red){
+        body.style.backgroundcolor="white";
+    }else{
+        body.style.backgroundcolor="red";
+    }
+    red=!red;
+
+});
+h2.addEventListener("mouseover",function(){
+    h2.style.color="red";
+});
+
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
